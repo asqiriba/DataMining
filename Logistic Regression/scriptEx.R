c5dd380c325ac8806841c086a044acacec35dadf
Hassan Asqiriba <7005262+asqiriba@users.noreply.github.com>
2020-09-25 18:34:38 -0500

    Add Logistic Regression folder

---
+++ Logistic Regression/scriptEx.R
@@ -0,0 +1,26 @@
+library(ISLR)
+summary(Weekly)
+
+cor(Weekly[, -9])
+
+attach(Weekly)
+plot(Volume)
+
+fit.glm = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, data = Weekly, family = binomial)
+summary(fit.glm)
+
+probs = predict(fit.glm, type = "response")
+pred.glm = rep("Down", length(probs))
+pred.glm[probs > 0.5] = "Up"
+table(pred.glm, Direction)
+
+train = (Year < 2009)
+Weekly.20092010 = Weekly[!train, ]
+Direction.20092010 = Direction[!train]
+fit.glm2 = glm(Direction ~ Lag2, data = Weekly, family = binomial, subset = train)
+summary(fit.glm2)
+
+probabilidad <- predict(fit.glm2, Weekly.20092010, type = "response")
+pred.glm2 <- rep("Down", length(probabilidad))
+pred.glm2[probabilidad > 0.5] <- "Up"
+table(pred.glm2, Direction.20092010)
